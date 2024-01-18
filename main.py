81a99c0e9b545958c0101cbe45f94eaa96140f2d
DeveloperUchida <kouei28poiou@icloud.com>
2024-01-18 10:13:58 +0900

    DataUpdate 2024/1/18

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def generate_terrain(width, height, scale):
     # 作ったオブジェクトを有効化
     bpy.context.view_layer.objects.active = obj
     obj.select_set(True)
-    
+
     # エンターキーを押すとメッシュを編集モードへ
     bpy.ops.object.mode_set(mode='EDIT')
 
@@ -25,4 +25,23 @@ def generate_terrain(width, height, scale):
     bm = bmesh.new()
 
     # テレインバーティクルを作成
-    
+    for x in range(width):
+        for y in range(height):
+            z = scale * random.uniform(0.0, 1.0)
+            bm.verts.new(x, y, z)
+
+    # 更新後バーティクルを同期させる
+    bm.to_mesh(mesh)
+    bm.free()
+    # 編集モードを解除
+    bpy.ops.object.mode_set(mode='OBJECT')
+    # テレインをスムーズ
+    bpy.ops.object.shade_smooth()
+
+    # テレインのスケール値
+    terrain_width = 50
+    terrain_hight = 50
+    terrain_scale = 10.0
+
+    # テレインを作成
+    generate_terrain(terrain_width, terrain_hight, terrain_scale)
